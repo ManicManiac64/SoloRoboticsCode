6777f51286077c922bd2d4ba67c2360ade5208fc
CringeCodin <74989150+CringeCodin@users.noreply.github.com>
2022-06-04 18:23:44 -0700

    Create constants.py
    Constants and stuff

---
+++ TestCode/robot/constants.py
@@ -0,0 +1,11 @@
+"""yeah, uh, as of now this file is kinda useless. but it's still important to have a place
+where you can put all your constants in my opinion, so here we go"""
+
+#controllers
+driverControllerPort = 0
+
+#motors
+FLMotorPort = 0
+BLMotorPort = 1
+FRMotorPort = 2
+BRMotorPort = 3
