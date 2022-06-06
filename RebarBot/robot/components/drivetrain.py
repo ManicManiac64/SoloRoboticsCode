6c000d8a0600e7547c8a4a6230fe912f71386eb1
CringeCodin <74989150+CringeCodin@users.noreply.github.com>
2022-06-06 00:38:53 -0700

    Formatting is important
    reality is very disappointing if you know where to look, so don't look
    
    this has been wise comments from james

--- RebarBot/robot/components/drivetrain.py
+++ RebarBot/robot/components/drivetrain.py
@@ -1,45 +1,47 @@
-#wpilib is kinda required when we do anything with robots in frc, sooooooo
+# wpilib is kinda required when we do anything with robots in frc, sooooooo
 import wpilib
-#wpilib.drive contains stuff great for tank drive! :D :D :D :D :D (those are happy faces)
+# wpilib.drive contains stuff great for tank drive! :D :D :D :D :D (those are happy faces)
 import wpilib.drive
-#ctre contains stuff for using TalonFXs on the robot, like TalonFX(). Kinda straightforward.
+# ctre contains stuff for using TalonFXs on the robot, like TalonFX(). Kinda straightforward.
 import ctre
-#magicbot, for magic bots!
+# magicbot, for magic bots!
 import magicbot
 
-#the actual class
+
+# the actual class
 class Drivetrain:
-  #define front left, back left, front right, back right motors
-  FLMotor: ctre.WPI_TalonFX
-  BLMotor: ctre.WPI_TalonFX
-  FRMotor: ctre.WPI_TalonFX
-  BRMotor: ctre.WPI_TalonFX
-  
-  #fun left and right values. these are class attributes but can still kinda sorta be used like
-  #an instance attribute, so who needs __init__ anyways, hahahahaha
-  left = magicbot.will_reset_to(0)
-  right = magicbot.will_reset_to(0)
-  
-  #setup for the drivetrain (woah!)
-  def setup(self):
-    #reset to defaults (just for fun)
-    self.FLMotor.restoreFactoryDefaults()
-    self.BLMotor.restoreFactoryDefaults()
-    self.FRMotor.restoreFactoryDefaults()
-    self.BRMotor.restoreFactoryDefaults()
-    
-    #back motors follow front motors in tank drive, this way we have less work :)
-    self.BLMotor.follow(self.FLMotor)
-    self.BRMotor.follow(self.FRMotor)
-    
-    #define differential drive (we'll use it for our tank drive)
-    self.drive = wpilib.drive.DifferentialDrive(self.FLMotor, self.FRMotor)
-  
-  def move(self, left, right):
-    #these values will be used in execute, making this a setter/control function
-    self.left = left if abs(left) >= 0.05 else 0 #this is a deadband. google it
-    self.right = right if abs(right) >= 0.05 else 0 #deadband, more like one direction, ha get it? 'cause they're a d-oh whatever, moving on
-  
-  #this is what actually does stuff, w-w-w-w-woah! cool beans B)
-  def execute(self):
-    self.drive.tankDrive(self.left, self.right)
+    # define front left, back left, front right, back right motors
+    FLMotor: ctre.WPI_TalonFX
+    BLMotor: ctre.WPI_TalonFX
+    FRMotor: ctre.WPI_TalonFX
+    BRMotor: ctre.WPI_TalonFX
+
+    # fun left and right values. these are class attributes but can still kinda sorta be used like
+    # an instance attribute, so who needs __init__ anyways, hahahahaha
+    left = magicbot.will_reset_to(0)
+    right = magicbot.will_reset_to(0)
+
+    # setup for the drivetrain (woah!)
+    def setup(self):
+        # reset to defaults (just for fun)
+        self.FLMotor.restoreFactoryDefaults()
+        self.BLMotor.restoreFactoryDefaults()
+        self.FRMotor.restoreFactoryDefaults()
+        self.BRMotor.restoreFactoryDefaults()
+
+        # back motors follow front motors in tank drive, this way we have less work :)
+        self.BLMotor.follow(self.FLMotor)
+        self.BRMotor.follow(self.FRMotor)
+
+        # define differential drive (we'll use it for our tank drive)
+        self.drive = wpilib.drive.DifferentialDrive(self.FLMotor, self.FRMotor)
+
+    def move(self, left, right):
+        # these values will be used in execute, making this a setter/control function
+        self.left = left if abs(left) >= 0.05 else 0  # this is a deadband. google it
+        self.right = right if abs(
+            right) >= 0.05 else 0  # deadband, more like one direction, ha get it? 'cause they're a d-oh whatever, moving on
+
+    # this is what actually does stuff, w-w-w-w-woah! cool beans B)
+    def execute(self):
+        self.drive.tankDrive(self.left, self.right)
--- RebarBot/robot/robot.py
+++ RebarBot/robot/robot.py
@@ -2,45 +2,46 @@
 main robot file (it does stuff)
 """
 
-#import the needed modules
+# import the needed modules
 import wpilib
 import magicbot
 import ctre
 import components.drivetrain
 import constants
 
+
 class MagicRobot(magicbot.MagicRobot):
-  """
+    """
   While we're here, I wanna thank the folks at Instempunks (Team 3966) down in Tennessee, 
   whose magic bot code from 2021 really helped me learn how this stuff works. No, they did not
   give me permission. Really taking 'steal from the best, invent the rest' to another level.
   """
-  
-  #beans. drivetrain is a Drivetrain, how cool is that woah 
-  drivetrain: components.drivetrain.Drivetrain
-  
-  #Create objects, wow! Yeah it's very self-explanatory
-  def createObjects(self):
-    
-    #motors
-    self.FLMotor = ctre.WPI_TalonFX(constants.FLMotorPort)
-    self.BLMotor = ctre.WPI_TalonFX(constants.BLMotorPort)
-    self.FRMotor = ctre.WPI_TalonFX(constants.FRMotorPort)
-    self.BRMotor = ctre.WPI_TalonFX(constants.BRMotorPort)
-    
-    #controller
-    self.driverController = wpilib.XboxController(constants.driverControllerPort)
-    
-  def teleopInit(self):
-    '''When teleop starts, this will be called'''
-    pass
-    #haha jkkkk
-    
-  def teleopPeriodic(self):
-    '''For every certain time interval teleop occurs, this is called'''
-    #MOVE AAAAAAAAAAAAAAAAAAAAAAA
-    self.drivetrain.move(-self.driverController.getLeftY(), -self.driverController.getRightY())
-    
+
+    # beans. drivetrain is a Drivetrain, how cool is that woah
+    drivetrain: components.drivetrain.Drivetrain
+
+    # Create objects, wow! Yeah it's very self-explanatory
+    def createObjects(self):
+        # motors
+        self.FLMotor = ctre.WPI_TalonFX(constants.FLMotorPort)
+        self.BLMotor = ctre.WPI_TalonFX(constants.BLMotorPort)
+        self.FRMotor = ctre.WPI_TalonFX(constants.FRMotorPort)
+        self.BRMotor = ctre.WPI_TalonFX(constants.BRMotorPort)
+
+        # controller
+        self.driverController = wpilib.XboxController(constants.driverControllerPort)
+
+    def teleopInit(self):
+        '''When teleop starts, this will be called'''
+        pass
+        # haha jkkkk
+
+    def teleopPeriodic(self):
+        '''For every certain time interval teleop occurs, this is called'''
+        # MOVE AAAAAAAAAAAAAAAAAAAAAAA
+        self.drivetrain.move(-self.driverController.getLeftY(), -self.driverController.getRightY())
+
+
 if __name__ == '__main__':
-  #RUN AAAAAAAAAAAAAAAAAAAAAAAAAA
-  wpilib.run(MagicRobot)
+    # RUN AAAAAAAAAAAAAAAAAAAAAAAAAA
+    wpilib.run(MagicRobot)
