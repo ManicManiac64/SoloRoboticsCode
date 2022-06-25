1d523ca12cf0fe88aefdc537a6f323befefaf68b
CringeCodin <74989150+CringeCodin@users.noreply.github.com>
2022-06-25 03:10:40 -0700

    Deleted stuff, added stuff
    Much more structured program now, though it's not even a quarter of the way done

--- LarryAttempt/robot/robot.py
+++ LarryAttempt/robot/robot.py
@@ -1,1 +1,18 @@
+import magicbot
+import ctre
 
+from components.swervedrive import SwerveDrive
+
+class Larry(magicbot.MagicRobot):
+    
+    self.drivetrain: SwerveDrive
+
+    def createObjects(self):
+        self.FLDrive = ctre.WPI_TalonFX(0)
+        self.FLSteer = ctre.WPI_TalonFX(4)
+        self.FRDrive = ctre.WPI_TalonFX(2)
+        self.FRSteer = ctre.WPI_TalonFX(6)
+        self.BLDrive = ctre.WPI_TalonFX(1)
+        self.BLSteer = ctre.WPI_TalonFX(5)
+        self.BRDrive = ctre.WPI_TalonFX(3)
+        self.BRSteer = ctre.WPI_TalonFX(7)
