f0d2057cd9f7cce4733c038b4a67ff4a74c56686
CringeCodin <74989150+CringeCodin@users.noreply.github.com>
2023-03-29 10:50:41 -0700

    Update robot.py

--- LarrySwerveStates2/robot.py
+++ LarrySwerveStates2/robot.py
@@ -1,1 +1,13 @@
+"""
+This is the robot.py file. It creates objects, executes code during teleop, and runs the robot.
+"""
 
+# import modules
+import wpilib
+import magicbot
+import ctre
+# import constants #
+# import components.swervedrive #
+"""
+class Larry(magicbot.MagicRobot):
+"""
